e3d667c3b0ad02103550ec99e04f39e24ac5fb3d
Peter Neher <p.neher@dkfz-heidelberg.de>
2019-02-06 10:15:33 +0100

    Update setup.py description

--- cmdint/__init__.py
+++ cmdint/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 1.1
+__version__ = "1.1.1"
 __copyright__ = "Copyright 2018, German Cancer Research Center (DKFZ), Division of Medical Image Computing"
 
 from cmdint.CmdInterface import CmdInterface
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='cmdint',
-      version='1.1',
+      version='1.1.1',
       description='Enables detailed logging of command line calls in a very lightweight manner.',
-      long_description='Enables detailed logging of command line calls in a very lightweight manner (coding wise). Also supports logging of python functions. CmdInterface logs information regarding the command version, command execution, the input and output, execution times, platform and python environment information as well as git repository information.',
+      long_description='CmdInterface enables detailed logging of command line and python experiments in a very lightweight manner (coding wise). It wraps your command line or python function calls in a few lines of code and logs everything you might need to reproduce the experiment later on or to simply check what you did a couple of years ago.',
       url='https://github.com/MIC-DKFZ/cmdint/',
       author='Peter F. Neher',
       author_email='p.neher@dkfz.de',

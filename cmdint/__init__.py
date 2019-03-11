5d5b66e34d051d06551de7e11579bc6fc065a301
Peter Neher <p.neher@dkfz-heidelberg.de>
2019-03-11 12:12:48 +0100

    Bump version: 1.3.0 → 1.3.1

--- cmdint/__init__.py
+++ cmdint/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __copyright__ = "Copyright 2018, German Cancer Research Center (DKFZ), Division of Medical Image Computing"
 
 from cmdint.CmdInterface import CmdInterface

e307756105d96b2cc684b026ea63e15f5c077c48
Peter Neher <p.neher@dkfz-heidelberg.de>
2022-02-09 11:05:56 +0100

    bump version to v3.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cmdint',
-      version='3.0.0',
+      version='3.0.1',
       description='Enables detailed logging of command line calls in a very lightweight manner.',
       long_description='CmdInterface enables detailed logging of command line and python experiments in a very lightweight manner (coding wise). It wraps your command line or python function calls in a few lines of code and logs everything you might need to reproduce the experiment later on or to simply check what you did a couple of years ago.',
       url='https://github.com/MIC-DKFZ/cmdint/',
